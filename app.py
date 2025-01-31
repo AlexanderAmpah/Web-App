feac2072aa4853306a3ae80bcfecd1d726032a68
Alexander Ampah <alexanderampah6@gmail.com>
2025-01-31 18:31:36 -0500

    Adding html file and fixing code in the others

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__, static_folder="static", template_folder="templates")
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('home.html')
 
 @app.route('/projects')
 def projects():
