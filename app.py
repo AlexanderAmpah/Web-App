eb7ef49500f2741423bebd80a6a4d103cea840df
Alexander Ampah <alexanderampah6@gmail.com>
2025-01-31 17:55:52 -0500

    Fixes to app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template
 from waitress import serve
 
-app = Flask(__name__, static_folder='static', template_folder='templates')
+app = Flask(__name__, static_folder="static", template_folder="templates")
 
 @app.route('/')
 def home():
