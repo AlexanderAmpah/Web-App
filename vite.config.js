3e66e02ce0ba71b0ba5d457bdd15764a9ca8c0e7
Alexander Ampah <alexanderampah6@gmail.com>
2025-04-04 15:32:42 -0400

    fixing code

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ export default defineConfig({
   server: {
     open: '/index.html', // Auto-opens in browser
   },
-  base: './', // Ensures relative asset paths in production build
+  base: '/', // Ensures relative asset paths in production build
   build: {
     outDir: 'dist', // Default, but can be customized
     assetsDir: 'assets', // Default
